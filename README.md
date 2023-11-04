6cfe8131a7e2c2449c12ce4d852bd93235e746a5
Landdoraine Chen <wending.chen2020@gmail.com>
2023-11-04 19:03:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# E-commerce backend
+  
+  [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]
+
+## Description
+
+The program is a e-commerce site by using Express.js API to allow Sequelize to interact with a MySQL database
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+
+## Installation
+
+Install NodeJS. Also install 'Express v4.17.1','mysql2 v2.1.0', 'dotenv v8.2.0', 'sequelize v6.32.1' packages. Also install Insomnia to test to test all API routes.
+
+
+## Usage
+
+To run the application, do the following actions:
+1. Go to folder db, and open integrated terminal and login to mysql using 'mysql -u root -p' command.
+2. 'source schema.sql' to create your database.
+3. Go to the main folder and open intergrated terminal.
+4. Execute 'npm run seed'.
+5. Execute 'npm start'.
+6. Open Insomnia to test API routes.
+
+## License
+
+License: [ISC License (ISC)](https://opensource.org/licenses/ISC)
+
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]
+
+
+## Credits
+
+https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+
+https://stackoverflow.com/questions/50354817/sequelize-decimal-data-save-with-2-decimal-points 
+
+
+
+
+
+
+
+
---
+++ routes/api/product-routes.js
@@ -0,0 +1,148 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+// The `/api/products` endpoint
+
+// get all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one product by id
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create new product
+router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tagIds: req.body.tag_id
+    });
+
+    if (req.body.tagIds && req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    res.status(200).json({ message: 'Product added'});
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// update product
+router.put('/:id', (req, res) => {
+  // update product data
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      if (req.body.tagIds && req.body.tagIds.length) {
+        
+        ProductTag.findAll({
+          where: { product_id: req.params.id }
+        }).then((productTags) => {
+          // create filtered list of new tag_ids
+          const productTagIds = productTags.map(({ tag_id }) => tag_id);
+          const newProductTags = req.body.tagIds
+          .filter((tag_id) => !productTagIds.includes(tag_id))
+          .map((tag_id) => {
+            return {
+              product_id: req.params.id,
+              tag_id,
+            };
+          });
+
+            // figure out which ones to remove
+          const productTagsToRemove = productTags
+          .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+          .map(({ id }) => id);
+                  // run both actions
+          return Promise.all([
+            ProductTag.destroy({ where: { id: productTagsToRemove } }),
+            ProductTag.bulkCreate(newProductTags),
+          ]);
+        });
+      }
+
+      return res.json(product);
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+
+    await ProductTag.destroy({ where: { product_id: req.params.id } });
+
+    await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({ message: 'Product deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
